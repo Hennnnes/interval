86ae95171645aab06ca3b46d35e2fcda115e1838
hennnnes <hroemmer@googlemail.com>
2021-02-08 21:12:34 +0100

    Copy code from codepen

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,106 @@
 <script>
-	const startTimer = event => {
-		const rounds = parseInt(document.querySelector('input[name="rounds"]').value, 10);
-		const round_duration = parseInt(document.querySelector('input[name="round_duration"]').value, 10);
-		const round_break = parseInt(document.querySelector('input[name="round_break"]').value, 10);
-		const break_duration = parseInt(document.querySelector('input[name="break_duration"]').value, 10);
+	// 1 Set = "Aufgabe"
+	// 1 Set hat Dauer und Pausedauer
+	// 1 Runde enthält X Sets
+	// Rundenhäufig
+	// Pause zwischen den Runden
 
+	let setsPerRound = 8;
+	let setDuration = 45;
+	let setBreakDuration = 15;
+	let rounds = 3;
+	let roundBreak = 60;
 
-		var roundInterval = setInterval(myTimer, 1000);
-		var setInterval = setInterval(myTimer, 1000);
+	let currentRound = 1;
+	let currentSet = 1;
+	let state = '';
+	let time = 0;
+	let active = false;
+	let disabled = false;
+	let done = false;
 
-		function myTimer() {
-			var d = new Date();
-			var t = d.toLocaleTimeString();
-			document.getElementById("demo").innerHTML = t;
+	async function run() {
+		disabled = false;
+		disabled = true;
+		for (currentRound = 1; currentRound <= rounds; currentRound++) {
+			for (currentSet = 1; currentSet <= setsPerRound; currentSet++) {
+				state = 'active';
+				active = true;
+				await startInterval(setDuration);
+
+				// prevent duplicate breaks
+				if (currentSet === setsPerRound) {
+					break;
+				}
+
+				state = 'break';
+				active = false;
+				await startInterval(setBreakDuration);
+			}
+
+			if (currentRound === rounds) {
+				break;
+			}
+			state = 'round-break';
+			active = false;
+			await startInterval(roundBreak);
 		}
+		done = true;
+		disabled = false;
+	}
+
+	async function startInterval(duration) {
+		return new Promise((resolve, reject) => {
+			const intervalInterval = setInterval(intervalCounter, 1000);
+			time = duration;
+			function intervalCounter() {
+				if (time == 1) {
+					clearInterval(intervalInterval);
+					return resolve();
+				}
+				time -= 1;
+			}
+		});
 	}
 </script>
 
-<main>
-	<h1>Intervals</h1>
-	<p>I choose to do <input type="number" name="rounds" value="8"> rounds of <input type="number" value="45" name="round_duration"> Seconds with <input type="number" value="15" name="round_break"> seconds break in between.</p>
-	<p>Between each round I wish a break of <input type="number" value="15" name="break_duration"> seconds.</p>
-	<button on:click={startTimer}>Let's go</button>
-	<p id="demo"></p>
+<main class="state--{state}">
+	<div class="container">
+		{#if done}
+			<h2>done 🎉</h2>
+		{/if}
+		{#if !disabled}
+		<form on:submit={run}>
+			<p>I choose to do <input type="number" bind:value={setsPerRound}> sets of <input type="number" bind:value={setDuration}> seconds with <input type="number" bind:value={setBreakDuration}> seconds break in between.</p>
+			<p>Between each of the <input type="number" bind:value={rounds}> rounds I need a break of <input type="number" bind:value={roundBreak}> seconds.</p>
+			<button on:click={run} {disabled}>start</button>
+		</form>
+		{:else}
+			<h2>{#if active}Let's Go!{:else}Pause{/if}</h2>
+			<p class="timer">{time}</p>
+			<ul>
+				<li>round: {currentRound} / { rounds }</li>
+				<li>set: {currentSet} / { setsPerRound }</li>
+			</ul>
+		{/if}
+	</div>
 </main>
 
 <style>
 	main {
-		max-width: 60ch;
+		min-height: 100vh;
 		margin: 0 auto;
 	}
 
+	.state--active {
+		background-color: #588b8b;
+	}
+	.state--break {
+		background-color: #c8553d;
+	}
+	.state--round-break {
+		background-color: #f28f3b;
+	}
 	input[type="number"] {
 		width: 7ch;
 		background-color: transparent;
@@ -42,4 +112,34 @@
 		-moz-box-shadow: none;
 		box-shadow: none;
 	}
+
+	button {
+		background: #000;
+		border: none;
+		color: #fff;
+		cursor: pointer;
+		font-size: inherit;
+		letter-spacing: .5px;
+		line-height: inherit;
+		margin: 1rem 0;
+		padding: .5rem 3rem;
+	}
+
+	.timer {
+		font-size: 5rem;
+	}
+
+	ul {
+		list-style: none
+	}
+
+	li {
+		display: inline-block;
+	}
+
+	li:not(:last-of-type):after {
+		content: '|';
+		margin: 0 1ch;
+	}
+
 </style>
